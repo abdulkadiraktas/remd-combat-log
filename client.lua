379568e2fe4095a6e346dd260152460c6de89f89
Abdulkadir AKTAŞ <iletisim@abdulkadiraktas.com.tr>
2023-07-22 13:52:37 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,6 @@
 local show3DText = false
-
-RegisterNetEvent("pixel_antiCL:show")
-AddEventHandler("pixel_antiCL:show", function()
+RegisterNetEvent("remd-combat-log:show")
+AddEventHandler("remd-combat-log:show", function()
     if show3DText then
         show3DText = false
     else
@@ -16,12 +15,10 @@ AddEventHandler("pixel_antiCL:show", function()
         end
     end
 end)
-
-RegisterNetEvent("pixel_anticl")
-AddEventHandler("pixel_anticl", function(id, crds, identifier, reason)
+RegisterNetEvent("remd-combat-log")
+AddEventHandler("remd-combat-log", function(id, crds, identifier, reason)
     Display(id, crds, identifier, reason)
 end)
-
 function Display(id, crds, identifier, reason)
     local displaying = true
 
@@ -29,43 +26,27 @@ function Display(id, crds, identifier, reason)
         Wait(Config.DrawingTime)
         displaying = false
     end)
-	
+
     Citizen.CreateThread(function()
         while displaying do
             Wait(5)
             local pcoords = GetEntityCoords(PlayerPedId())
-            if GetDistanceBetweenCoords(crds.x, crds.y, crds.z, pcoords.x, pcoords.y, pcoords.z, true) < 15.0 and show3DText then
-                DrawText3DSecond(crds.x, crds.y, crds.z+0.15, "Player Left Game")
-                DrawText3D(crds.x, crds.y, crds.z, "ID: "..id.." ("..identifier..")\nReason: "..reason)
+            if show3DText and #(crds - pcoords) < 15.0 then
+                drawText3d(crds.x, crds.y, crds.z+0.15, "Player Left Game", Config.AlertTextColor)
+                drawText3d(crds.x, crds.y, crds.z, "ID: "..id.." ("..identifier..")\nReason: "..reason,Config.TextColor)
             else
                 Citizen.Wait(2000)
             end
         end
     end)
 end
-
-function DrawText3DSecond(x,y,z, text)
-    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
-    SetTextScale(0.45, 0.45)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(Config.AlertTextColor.r, Config.AlertTextColor.g, Config.AlertTextColor.b, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(1)
-    AddTextComponentString(text)
-    DrawText(_x,_y)
-end
-
-function DrawText3D(x,y,z, text)
-    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
-    SetTextScale(0.45, 0.45)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(Config.TextColor.r, Config.TextColor.g, Config.TextColor.b, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(1)
-    AddTextComponentString(text)
-    DrawText(_x,_y)
+function drawText3d(x, y, z, text, color)
+	local onScreen, screenX, screenY = GetScreenCoordFromWorldCoord(x, y, z)
+	if onScreen then
+        SetTextScale(0.35, 0.35)
+        SetTextFontForCurrentCommand(1)
+        SetTextColor(color.r,color.g,color.b, 255)
+        SetTextCentre(1)
+        DisplayText(CreateVarString(10, "LITERAL_STRING", text), screenX, screenY)
+	end
 end
